f1651d1dddb9b63fdacd03d7b31bf01ae5d1eafb
Krisjel <krisjelannerecolcolin@gmail.com>
2024-10-21 20:11:58 +0800

    Added real-time weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,32 @@ function updateWeather (response){
     let temperatureElement = document.querySelector("#weather-app-temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#weather-app-city");
-cityElement.innerHTML = response.data.city;
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let speedElemenet = document.querySelector("#speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date (response.data.time * 1000);
 
+
+
+    cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    speedElemenet.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
+}
+function formatDate(date) {
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days [date.getDay()];
+
+    if (minutes < 10 ) {
+        minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity (city){
@@ -25,3 +48,5 @@ searchCity(searchInput.value);
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Manila");
