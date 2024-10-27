fc20ebba5e6d5cbc53690829ebe769a4035678fd
Krisjel <krisjelannerecolcolin@gmail.com>
2024-10-27 19:23:36 +0800

    Revise the JS and added the forecast API integration

--- src/index.js
+++ src/index.js
@@ -48,32 +48,37 @@ function handleSearchSubmit(event) {
 searchCity(searchInput.value);
 }
 
+function formatDay (timestamp) {
+let date = new Date(timestamp * 1000);
+let days = ["Sun", "Mon", "Tue", "Wed","Thu", "Fri", "Sat"];
+
+return days[date.getDay()];
+}
 function getForecast(city){
 let apiKey = "af645t00bbc7e3co535a2e952451ad49";
 let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
 axios(apiUrl).then(displayForecast);
 }
-function displayForecast(response){
-    let forecastElement = document.querySelector("#forecast");
-
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+function displayForecast(response){    
     let forecastHtml = "";
 
-    days.forEach(function(day){
+    response.data.daily.forEach(function(day, index){
+        if (index < 5) {
         forecastHtml = forecastHtml + `
         <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon"> 🌤️ </div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
         <div class="weather-forecast-temperature">
-        <strong>15°</strong>
+        <strong>${Math.round(day.temperature.maximum)}°</strong>
         </div>
-        <div class="weather-forecast-temperature">9°</div>
+        <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
         </div>
         </div>
         `;
+    };
     });
-
+    let forecastElement = document.querySelector("#forecast");
     forecastElement.innerHTML = forecastHtml;
 }
 
@@ -81,4 +86,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Manila");
-getForecast("Manila");
+
